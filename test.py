b341b36cb7e8cbf4ccfc62d855e532b63b5eb11c
gpamfilis <gpamfilis@gmail.com>
2017-02-22 17:52:59 +0200

    problems with communication

--- test.py
+++ test.py
@@ -34,8 +34,9 @@ def create_deltio():
         height = 50 * (n_orders+3)
         h = 50 * n_orders*3
         head = 20
+        left = 40
         back_ground_color = (255, 255, 255)
-        font_size = 25
+        font_size = 15
         font_color = (0, 0, 0)
         im = Image.new("RGB", (width, height), back_ground_color)
         unicode_font = ImageFont.truetype('./fonts/arial.ttf', font_size)
@@ -44,9 +45,9 @@ def create_deltio():
         items = json["items"]
         draw = ImageDraw.Draw(im)
 
-        draw.text((20, head), items[0]["table_name"], font=unicode_font, fill=font_color)
-        draw.text((60, head), items[0]["datetime"], font=date_font, fill=font_color)
-        draw.text((250, head), str(i), font=date_font, fill=font_color)
+        draw.text((20+left, head), items[0]["table_name"], font=unicode_font, fill=font_color)
+        draw.text((60+left, head), items[0]["datetime"], font=date_font, fill=font_color)
+        draw.text((250+left, head), str(i), font=date_font, fill=font_color)
         gap = h/(n_orders*3)
         # draw.text((10, 0+gap), " ", font=unicode_font, fill=font_color)
 
@@ -54,7 +55,7 @@ def create_deltio():
         for item in items:
             name = item["name"]
             print(name)
-            draw.text((20, 2*head + d_line), "x" + str(item["quantity"]) + " " + name[:14], font=unicode_font, fill=font_color)
+            draw.text((20+left, 2*head + d_line), "x" + str(item["quantity"]) + " " + name[:14], font=unicode_font, fill=font_color)
             d_line += gap
         im.save("./orders_pic/id_"+str(i)+"_text.png")
 
