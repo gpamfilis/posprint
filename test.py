ab314f31459e2356d6cfe509bdc39e3a49fc58bc
gpamfilis <gpamfilis@gmail.com>
2017-02-22 17:07:18 +0200

    save all orders to image file

--- test.py
+++ test.py
@@ -21,10 +21,11 @@ def get_orders_to_print():
 
 
 def create_deltio():
+
     ids = get_orders_to_print()
 
     for i in ids:
-
+    # for i in [3]:
         json = get_order(i)
 
         items = json["items"]
@@ -32,11 +33,11 @@ def create_deltio():
         n_orders = len(items)
         print(n_orders)
         width = 350
-        height = 50 * (n_orders+1)
+        height = 50 * (n_orders)
         h = 50 * n_orders*3
 
         back_ground_color = (255, 255, 255)
-        font_size = 30
+        font_size = 20
         font_color = (0, 0, 0)
         im = Image.new("RGB", (width, height), back_ground_color)
         unicode_font = ImageFont.truetype('./fonts/arial.ttf', font_size)
@@ -46,7 +47,7 @@ def create_deltio():
         draw = ImageDraw.Draw(im)
 
         draw.text((10, 0), items[0]["table_name"], font=unicode_font, fill=font_color)
-        draw.text((50, 5), items[0]["datetime"], font=date_font, fill=font_color)
+        draw.text((50, 0), items[0]["datetime"], font=date_font, fill=font_color)
         gap = h/(n_orders*4)
         # draw.text((10, 0+gap), "Table Ndame", font=unicode_font, fill=font_color)
 
@@ -54,7 +55,7 @@ def create_deltio():
         for item in items:
             name = item["name"]
             print(name)
-            draw.text((10, d_line), "x" + str(item["quantity"]) + " " + name, font=unicode_font, fill=font_color)
+            draw.text((10, d_line), "x" + str(item["quantity"]) + " " + name[:19], font=unicode_font, fill=font_color)
             d_line += gap
         im.save("./orders_pic/id_"+str(i)+"_text.png")
 
