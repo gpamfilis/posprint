66a0ab18ab7619daa4c327a81771498477dce372
gpamfilis <gpamfilis@gmail.com>
2017-02-22 16:20:31 +0200

    added a working "ugly" example of displaying greek characters.

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import requests
 
 
 def get_order(order_id):
-    data = requests.get("http://www.e-orders.org/api/app/order?order_id="+str(order_id))
+    data = requests.get("http://www.e-orders.org/api/app/order?order_id=" + str(order_id))
     json = data.json()
     print(json)
     return json
@@ -28,20 +28,26 @@ def create_deltio(order_id):
     font_color = (0, 0, 0)
     im = Image.new("RGB", (width, height), back_ground_color)
     unicode_font = ImageFont.truetype('./fonts/arial.ttf', font_size)
+    date_font = ImageFont.truetype('./fonts/arial.ttf', 20)
 
     items = json["items"]
     draw = ImageDraw.Draw(im)
-    d_line = font_size
-    draw.text((10, 0), "Table Name", font=unicode_font, fill=font_color)
+
+    draw.text((10, 0), items[0]["table_name"], font=unicode_font, fill=font_color)
+    draw.text((50, 5), items[0]["datetime"], font=date_font, fill=font_color)
+    gap = h/(n_orders*4)
+    # draw.text((10, 0+gap), "Table Name", font=unicode_font, fill=font_color)
+
+    d_line = font_size  # +(gap*1)
     for item in items:
         name = item["name"]
         print(name)
         # unicode_text = unicode(name, "utf-8")
-        draw.text((10, d_line), "x" + str(item["quantity"]) + " "+name, font=unicode_font, fill=font_color)
-        d_line += h/(n_orders*4)
+        draw.text((10, d_line), "x" + str(item["quantity"]) + " " + name, font=unicode_font, fill=font_color)
+        d_line += gap
     im.save("text.png")
 
 
 if __name__ == '__main__':
-    create_deltio(10)
+    create_deltio(7)
 
