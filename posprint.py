02e6481adf5974ecf517b76b62bd871071f330de
gpamfilis <gpamfilis@gmail.com>
2017-02-25 00:48:25 +0200

    added a way to iterate over the contents

---
+++ posprint.py
@@ -0,0 +1,19 @@
+__author__ = 'gpamfilis'
+
+import thermal
+
+if __name__ == '__main__':
+    import sys, os
+    if len(sys.argv) == 2:
+        serial_port = sys.argv[1]
+    else:
+        serial_port = thermal.ThermalPrinter.SERIALPORT
+
+    p = thermal.ThermalPrinter(serialport=serial_port)
+    for fil in os.listdir("./order_txt"):
+        f = open("./order_txt/"+fil)
+        lines = f.readline()
+        f.close()
+        for line in lines:
+            p.print_text(line)
+        p.linefeed(5)
--- test_with_translator.py
+++ test_with_translator.py
@@ -1,19 +1,20 @@
 import requests
+import greeklish
 
 __author__ = 'gpamfilis'
 
-
-import greeklish
 
 def get_order(order_id):
     data = requests.get("http://www.e-orders.org/api/app/order?order_id=" + str(order_id))
     json = data.json()
     return json
 
+
 def get_orders_to_print():
     data = requests.get("http://www.e-orders.org/api/printer/orders-print?store_id=" + str(1))
     json = data.json()["ids"]
     return json
+
 
 def create_deltio():
     ids = get_orders_to_print()
@@ -23,7 +24,7 @@ def create_deltio():
         f = open("./order_txt/id_" + str(i) + "text.txt", "w")
         items = json["items"]
         table_name = items[0]["table_name"]
-        f.write(table_name + "  "+ items[0]["datetime"])
+        f.write(table_name + "  " + items[0]["datetime"])
         f.write("\n")
         for item in items:
             name = "x" + str(item["quantity"]) + " " + greeklish.main(item["name"].encode("utf-8"))
@@ -32,9 +33,10 @@ def create_deltio():
             if len(item["contents"]) == 0:
                 pass
             else:
-                # contents = item["contents"]
-                for content in range(4):  # contents
-                    f.write("    " + "cheese")
+                contents = item["contents"]
+                for content in contents:
+                    # find a way to range them on a line.
+                    f.write("    " + greeklish.main(content))
                     f.write("\n")
         f.close()
 
