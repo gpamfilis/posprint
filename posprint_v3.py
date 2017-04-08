8669ab6b7b87ed9795d0d41d2bef9ed5121056d5
George Pamfilis <gpamfilis@gmail.com>
2017-04-08 23:00:30 +0300

    test

---
+++ posprint_v3.py
@@ -0,0 +1,130 @@
+# coding: utf-8
+import requests
+import sys
+import thermal
+import time
+from network import PrinterNetCalls, InternetConnection
+import sys
+import os
+import greeklish
+__author__ = 'gpamfilis'
+import os
+import shutil
+from utils import Create
+
+
+pnc = PrinterNetCalls()
+
+ids = pnc.get_orders_to_print()
+
+
+def create_deltio(id_):
+    try:
+        # int.check_internet()
+        json = pnc.get_order(id_)
+    except Exception, e:
+        print("This is the create_deltio exception", e)
+
+    if json is None:
+        return None
+    else:    
+        order = []
+
+        items = json["items"]
+        table_name = items[0]["table_name"]
+
+        order.append(table_name + "  " + items[0]["datetime"])
+        order.append(str(id_))
+        for item in items:
+            name = "x" + str(item["quantity"]) + " " + greeklish.main(item["name"].encode("utf-8"))[:28]
+            order.append(name)
+            if len(item["contents"]) == 0:
+                pass
+            else:
+                contents = item["contents"]
+                for content in contents:
+                    # find a way to range them on a line.
+                    name = content["content_name"].encode("utf-8")
+                    greeklish_content = greeklish.main(name)
+                    cont = "    " + greeklish_content
+                    order.append(cont)
+        return order
+
+
+def read_items(fil):
+    print(fil)
+    f = open("./order_txt/" + fil)
+    lines = f.readlines()
+    f.close()
+    return lines
+
+
+def printer_paper_status(p):
+    paper_status = [p.has_paper() for i in range(10)][-1]
+    while paper_status is False:
+        paper_status = p.has_paper()
+        print("no paper in printer!")
+        time.sleep(2)
+        if paper_status:
+            print("paper in printer")
+            time.sleep(5)
+            break
+
+
+def check_printer_status(p):
+    printer_status = p.has_printed()
+    while printer_status is False:
+        print("not done printing")
+        printer_status = [p.has_printed() for i in range(10)][-1]
+        time.sleep(2)
+        if printer_status:
+            print("printer free")
+            time.sleep(5)
+            break
+
+
+
+def print_order2(id_, order_list):
+    pnc = PrinterNetCalls()
+    if len(sys.argv) == 2:
+        serial_port = sys.argv[1]
+    else:
+        serial_port = thermal.ThermalPrinter.SERIALPORT
+    p = thermal.ThermalPrinter(serialport=serial_port)
+
+    print("Attempting to print ord er_id: ", id_)
+    lines = order_list
+    print("initial paper check")
+    printer_paper_status(p)
+    print("---NEW--ORDER----!!")
+    print("printing order_id", id_)
+    p.print_text("---NEW--ORDER----!!")
+    p.print_text("\n")
+    for line in lines:
+        printer_paper_status(p)
+        print("printing line", line[:])
+        p.print_text(line[:]+'\n')
+
+    p.linefeed(5)
+
+    check_printer_status(p)
+
+    pnc.post_that_order_was_printed(int(id_))
+    print("Sending message to server for order id: ", id_)
+    time.sleep(5)
+
+
+if __name__ == '__main__':
+    # int = InternetConnection()
+    pnc = PrinterNetCalls()
+    create = Create()
+    while True:
+        pnc.check_internet()
+        ids = pnc.get_orders_to_print()
+        for id_ in ids:
+            order = create_deltio(id_)
+            order_list = create.print_order2(order)
+            if order_list is None:
+                pass
+            else:
+                print_order(id_, order_list)
---
+++ utils.py
@@ -0,0 +1,76 @@
+from string import maketrans, translate
+
+
+def greeklishgrtoen(strings):
+    try:
+        text = strings
+        text = text.decode('utf-8')
+        text = text.encode('iso-8859-7', 'replace')
+        from_chars = u'áâãäåæçèéêëìíîïðñóòôõö÷øùÜÝÞßúÀüýûàþÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÓÔÕÖ×ØÙ¶¸¹ºÚ¼¾Û¿'.encode('latin1')
+        to_chars = u'abgdezh8iklmn3oprsstufxywaehiiiouuuwABGDEZH8IKLMNJOPRSTYFXCWAEHIIOUUW'.encode('latin1')
+        trantab = maketrans(from_chars, to_chars)
+        # trantab = dict((ord(a), b) for a, b in zip(from_chars, to_chars))
+        text = translate(text, trantab)
+        return text.upper()
+    except Exception, e:
+        return e
+
+
+class Create(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def order(json_object):
+        try:
+            json = json_object
+        except Exception, e:
+            print("This is the create_deltio exception", e)
+
+        if json is None:
+            return None
+        else:
+            order = []
+            items = json["items"]
+            # print(items)
+            table_name = items["items"][0]["table_name"]
+            order_id = items["order_id"]
+            # print(table_name, order_id)
+            order.append(table_name + "  " + items["datetime"])
+            order.append(str(order_id))
+            # print(items)
+            for item in items["items"]:
+                # print(item)
+                name = "x" + str(item["quantity"]) + " " + greeklishgrtoen(item["name"].encode("utf-8"))[:28]
+                order.append(name)
+                if len(item["contents"]) == 0:
+                    pass
+                else:
+                    contents = item["contents"]
+                    for content in contents:
+                        if (content["changed"] == 1) and (content["default"] == 1):
+                            name = content["content_name"].encode("utf-8")
+                            greeklish_content = greeklishgrtoen(name)
+                            cont = "    NOT  " + greeklish_content
+                            order.append(cont)
+                        elif (content["changed"] == 1) and (content["default"] == 0):
+                            name = content["content_name"].encode("utf-8")
+                            greeklish_content = greeklishgrtoen(name)
+                            cont = "    YES  " + greeklish_content
+                            order.append(cont)
+            return order
+
+    @staticmethod
+    def checkout(json_object):
+        checkout_list = []
+        order_items = json_object["items"]
+        grand_total = 0
+        for order in order_items:
+            for item in order["items"]:
+                name = str(item["quantity"]) + "  " + greeklishgrtoen(item["name"]).encode("utf-8")
+                checkout_list.append(name)
+            total = "subtotal  " + str(order["total"])
+            grand_total += order["total"]
+            checkout_list.append(total)
+        checkout_list.append("Total:    "+str(round(grand_total, 2)))
+        return checkout_list
