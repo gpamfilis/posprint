5e348dca08af9d53a78168583cca6b33d0f75c06
George Pamfilis <gpamfilis@gmail.com>
2017-07-16 02:03:03 +0300

    changed the characters per line to 27

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /
-echo "I AM ALIIIIIIIVE //n//n//n//n//n//n" > /dev/ttyAMA0
+echo "I AM ALIIIIIIIVE //n//n//n///n//n//n" > /dev/ttyAMA0
 cd home/pi/git/posprint
 git fetch --all
 git reset --hard origin/master
